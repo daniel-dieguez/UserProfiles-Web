fcb66b8d9a8b8df48d2e6138938d4d0876b84b82
daniel-dieguez <danguez2001@gmail.com^C>
2024-07-29 13:26:11 -0600

    Se realizo el consumo de imagenes a base de base64 con el consumo adecuado de api get axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import Form from './components/Form'
 import NewProfile from './utils/NewCart'
+import Style from '../src/style/style.module.css' 
+import Prueba from './parctica/prueba'
+import UpLoad from './components/UpLoad'
+import PruebaCart from './parctica/PruebaCart'
 
-export default function App() {
+export default function App( title) {
   return (
     <div>
+    <div className={Style.body}>
+      <Prueba title="todo ok bro"></Prueba>
+      <span>asd</span>
         <Form></Form>
-        <NewProfile></NewProfile>
+        <NewProfile ></NewProfile>
+        <PruebaCart></PruebaCart>
+        <UpLoad></UpLoad>
 
+        </div>
     </div>
   )
 }
